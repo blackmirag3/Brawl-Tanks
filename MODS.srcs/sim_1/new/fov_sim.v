5272b06651b9afc78c2566cc0ea03e371c44d3fe
shui <78994143+blackmirag3@users.noreply.github.com>
2024-04-02 00:17:36 +0800

    Fix indentation and bug

--- MODS.srcs/sim_1/new/fov_sim.v
+++ MODS.srcs/sim_1/new/fov_sim.v
@@ -24,8 +24,13 @@ module fov_sim(
     );
     reg CLOCK = 0;
     reg [15:0] x_out, y_out;
-    test fov(CLOCK, 255, 100, 100, 0, -100, x_out, y_out);
+    reg [7:0] x_in = 100, y_in = 100, x2_in = -100, y2_in = -100;
+    reg [7:0] angle = 0;
+    test fov(CLOCK, angle, x_in, y_in, x2_in, x2_in, -100, x_out, y_out);
     always begin
-            #1 CLOCK = ~CLOCK;
-        end
+        #1 CLOCK = ~CLOCK;
+    end
+    always @ (posedge CLOCK) begin
+        angle <= angle + 1;
+    end
 endmodule
